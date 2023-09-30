a845889f9912d98bc0c1f3d29147590591c9c98c
Louis Peyrondet <louis.peyrondet@hotmail.fr>
2023-09-30 02:59:57 +0200

    Added block timer

--- utils.c
+++ utils.c
@@ -5,3 +5,7 @@
 
 
 
+long get_elapsed_time(struct timeval start, struct timeval end)
+{
+	return ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec))/1000.0;
+}
--- utils.h
+++ utils.h
@@ -5,13 +5,24 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <signal.h>
+#include <sys/time.h>
 
 
 // https://nullprogram.com/blog/2022/06/26/#addendum-dont-exit-the-debugger
 #define assert(c) if (!(c)) __builtin_trap();
 
 //improve with a logger
-#define check_malloc(c) if(!(c)) fprintf(stderr, "Malloc failed !\n"); exit(EXIT_FAILURE); 
+#define check_malloc(c) if(!(c)){ fprintf(stderr, "Malloc failed !\n"); exit(EXIT_FAILURE); } 
+
+long get_elapsed_time(struct timeval start, struct timeval end);
+
+#define TIME_BLOCK_EXEC(msg, ...) do {                                           \
+    struct timeval __start, __end;                                               \
+    gettimeofday(&__start, NULL);                                                \
+    __VA_ARGS__                                                                  \
+    gettimeofday(&__end, NULL);                                                  \
+    printf("%s execution time: %ldms\n", msg, get_elapsed_time(__start, __end)); \
+} while(0);
 
 
 #endif // _UTILS_H__
