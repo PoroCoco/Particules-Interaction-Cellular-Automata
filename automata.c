3d230aef62a7a6ed1e526dd578b343903fabb90e
Louis Peyrondet <louis.peyrondet@hotmail.fr>
2023-09-30 03:01:01 +0200

    Automata base

--- automata.c
+++ automata.c
@@ -1,15 +1,14 @@
 #include "automata.h"
-#include "particules.h"
-#include "utils.h"
 
-#define autom_idx(autom, row, col) ((((autom)->width)*(row)) + (col))
+#define aut_idx(autom, row, col) ((((autom)->width)*(row)) + (col))
 
 
 typedef struct automata{
     uint32_t width;
     uint32_t height;
-    uint64_t current_frame;
+    uint32_t current_frame;
     particule *world;
+    uint32_t world_size;
 
 
 } automata;
@@ -22,13 +21,55 @@ automata* automata_init(uint32_t width, uint32_t height){
     autom->current_frame = 0;
     autom->world = malloc(sizeof(*autom->world) * width * height);
     check_malloc(autom->world);
+    autom->world_size = width * height;
+
+    for (uint32_t i = 0; i < autom->world_size; i++)
+        autom->world[i] = particule_create(TYPE_AIR);
+    
+
+    for (size_t i = 0; i < width; i++)
+        autom->world[aut_idx(autom, 10, i)] = particule_create(TYPE_SAND);
+    
+    for (size_t i = 0; i < height; i++)
+        autom->world[aut_idx(autom, i, 500)] = particule_create(TYPE_SAND);
 
 
     return autom;
 }
 
-void automata_update(automata* autom);
+void automata_update(automata* autom){
+    for (uint32_t i = autom->world_size -1 ; i != 0; i--)
+    {
+    
+        if(autom->world[i].type == TYPE_SAND){
+            if (i + autom->width < autom->world_size && autom->world[i + autom->width].type == TYPE_AIR){
+                particule tmp = autom->world[i + autom->width];
+                autom->world[i + autom->width] = autom->world[i];
+                autom->world[i] = tmp;
+            }else if(i + autom->width + 1 < autom->world_size && autom->world[i + autom->width + 1].type == TYPE_AIR){
+                particule tmp = autom->world[i + autom->width + 1];
+                autom->world[i + autom->width + 1] = autom->world[i];
+                autom->world[i] = tmp;
+            }else if(i + autom->width - 1 < autom->world_size && autom->world[i + autom->width - 1].type == TYPE_AIR){
+                particule tmp = autom->world[i + autom->width - 1];
+                autom->world[i + autom->width - 1] = autom->world[i];
+                autom->world[i] = tmp;
+            }
+        }
+    }
+    return;
+}
 
 void automata_cleanup(automata* autom){
+    if (autom) free(autom->world);
     free(autom);
+}
+
+
+uint32_t automata_get_size(const automata* autom){
+    return autom->world_size;
+}
+
+enum particule_type automata_get_particule_type(const automata* autom, uint32_t index){
+    return autom->world[index].type;
 }
--- automata.h
+++ automata.h
@@ -1,12 +1,19 @@
 #ifndef _AUTOMATA_H__
 #define _AUTOMATA_H__
 
+#include "particules.h"
+#include "utils.h"
+
 typedef struct automata automata;
 
-automata* automata_init(void);
+automata* automata_init(uint32_t width, uint32_t height);
 
 void automata_update(automata* autom);
 
 void automata_cleanup(automata* autom);
+
+uint32_t automata_get_size(const automata* autom);
+
+enum particule_type automata_get_particule_type(const automata* autom, uint32_t index);
 
 #endif // _AUTOMATA_H__
